ed2ca6313a0149aba450745cbcc3052f9f6cfe08
FreddyOs <94193132+FreddyOs@users.noreply.github.com>
2023-07-06 21:53:33 +0200

    did some bug fixes

--- Assets/REBAScoreHUD.cs
+++ Assets/REBAScoreHUD.cs
@@ -3,7 +3,7 @@ using UnityEngine.UI;
 
 public class REBAScoreHUD : MonoBehaviour
 {
-    private Text scoreText;
+    public Text scoreText;
 
     // The size of the font when the REBA score is 1
     public int initialFontSize = 14;
@@ -11,7 +11,7 @@ public class REBAScoreHUD : MonoBehaviour
     // The amount to increase the font size for each increase in REBA score
     public int fontSizeIncreasePerPoint = 2;
 
-    private void Start()
+    private void Start()  // Changed from Start to Awake
     {
         scoreText = GetComponent<Text>();
         UpdateScoreText(0);  // Initialize with 0 score
